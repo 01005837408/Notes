65874174aea06c66cc0ead4597f9ffcc5f392c24
mohamed_hussein <mohamedhusseinmans@gmail.com>
2025-12-16 21:42:26 +0200

    finish edit note view

--- lib/presentation/widget/edit_note_view_body.dart
+++ lib/presentation/widget/edit_note_view_body.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_app/presentation/widget/custom_appBar.dart';
+import 'package:hive_app/presentation/widget/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
       child: Column(
         children: [
           CustomAppBar(
             title: "Edit Note",
-            icon:Icons.check,
+            icon: Icons.check,
+          ),
+          SizedBox(
+            height: 40.h,
+          ),
+          CustomTextField(
+            hintText: 'Title',
+          ),
+          SizedBox(
+            height: 16.h,
+          ),
+          CustomTextField(
+            hintText: 'Description',
+            maxLines: 5,
           ),
         ],
       ),
